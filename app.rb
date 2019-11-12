aada6823ab4c5ae802337308d9cb3595ee923df0
Brandon Brasson <rocksong622@gmail.com>
2019-11-12 17:14:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,10 +6,6 @@ class App < Sinatra::Base
     erb :food_form
   end
 
-  get '/food_form' do
-   erb :food_form
- end
-
  post '/food' do
    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
  end
